dcdf876c290cece95412b1f09d64c31b4288c1cd
NItin Kharayat <164913459+nitinkharayat@users.noreply.github.com>
2026-08-23 22:16:20 +0530

    classes and object

--- oops(with python)/foundation/class_and_object_using_parameters.py
+++ oops(with python)/foundation/class_and_object_using_parameters.py
@@ -1,15 +1,11 @@
-class Students:
-    roll_no = 0
-    name = ""
-    age = 0
-    gender = 0 
-    def set_details(self,Name : str ,Age : int ,Roll_Number:int,Gender:str):
+class Students:#No attributes
+    def __init__(self,Name : str ,Age : int ,Roll_Number:int,Gender:str) -> None:
         self.name = Name
         self.age = Age
         self.roll_no = Roll_Number
         self.gender = Gender
-    def get_details(self):
+    def get_details(self) -> None:
         print(f"Name = {self.name}\nAge = {self.age}\nRoll_no = {self.roll_no}\nGender={self.gender}")
-student1 = Students()
-student1.set_details("Name",22,24,"male")
+
+student1 = Students("Nitin",22,24,"male")
 student1.get_details()

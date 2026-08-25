30cff666738e1cfcf3c96c7260b3140f55637df8
NItin Kharayat <164913459+nitinkharayat@users.noreply.github.com>
2026-08-26 02:00:59 +0530

    day01b

---
+++ c++/Operators(pre defined)/sizeof.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int a = 0;
+int main(){
+    int b  = 18;
+//  size of operator is used to find the size of the object
+    cout<<sizeof(b<<endl;
+    cout<<sizeof(bool)<<endl;
+    cout<<sizeof(float);
+    
+    return 0;
+}

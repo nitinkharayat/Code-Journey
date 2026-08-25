1ec446984a5f8042736511daddeb937b2ad922d9
NItin Kharayat <164913459+nitinkharayat@users.noreply.github.com>
2026-08-26 01:38:13 +0530

    day01

--- c++/variables/variables.cpp
+++ c++/variables/variables.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int x;
-    // x has now garbage value(random value)
+    int x;//int is decleration of the data type
     // here int denote the datatype
     x = 13;//now we have assigned value to the x 
     cout << x;//use to output 
-    return 0;
+    return 0;//= is a aggingment operator 
 }

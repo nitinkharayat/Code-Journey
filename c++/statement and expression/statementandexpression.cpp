13db75ac3313274b606ef89a0c14e55fdcc1f136
NItin Kharayat <164913459+nitinkharayat@users.noreply.github.com>
2026-08-26 15:40:00 +0530

    day01expression

---
+++ c++/statement and expression/statementandexpression.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+string s = "hello";
+int main() {
+ int a,b,c;//this whole is a statement
+ b = 12;// whole is expression but 12 is statement 
+ c = 22;
+ a = b+c;//b+c is expression
+
+}

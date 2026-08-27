89e3e957af7708108585baff0f5da0eae8c1ed89
NItin Kharayat <164913459+nitinkharayat@users.noreply.github.com>
2026-08-27 22:15:03 +0530

    day020

---
+++ c++/Operators(pre defined)/Arthematicoperators.cpp
@@ -0,0 +1,6 @@
+///There are many type of operator in c++ but arthematic operator are of givnen type
+/*
+1.+
+2.-
+3.*
+4.*/
--- c++/statement and expression/statementandexpression.cpp
+++ c++/statement and expression/statementandexpression.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 string s = "hello";
 int main() {
- int a,b,c;//this whole is a statement
- b = 12;// whole is expression but 12 is statement 
- c = 22;
- a = b+c;//b+c is expression
+    int a,b,c;//this whole is a statement
+    b = 12;// whole is expression but 12 is statement 
+    c = 22;
+    a = b+c;//b+c is expression
 
 }
